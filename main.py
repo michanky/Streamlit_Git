380b82a1c5bd1104ab64961c270e1321dea735a0
michanky <michaelngky@gmail.com>
2021-10-12 00:21:04 +0800

    Covid data

--- main.py
+++ main.py
@@ -2,18 +2,12 @@ import streamlit as st
 import pandas as pd
 import base64
 import matplotlib.pyplot as plt
-import seaborn as sns
-import numpy as np
-import yfinance as yf
 st.set_option('deprecation.showPyplotGlobalUse', False)
 
-st.title('S&P 500 App')
+st.title('Covid-19 Data')
 
 st.markdown("""
-This app retrieves the list of the **S&P 500** (from Wikipedia) and its corresponding **stock closing price** (year-to-date)!
-* **Python libraries:** base64, pandas, streamlit, numpy, matplotlib, seaborn
-* **Data source:** [Wikipedia](https://en.wikipedia.org/wiki/List_of_S%26P_500_companies). 
-Credited to dataprofessor @ http://youtube.com/dataprofessor
+This app displays the information of Covid-19 cases around the world
 """)
 
 st.sidebar.header('User Input Features')
@@ -24,73 +18,55 @@ st.sidebar.header('User Input Features')
 
 @st.cache
 def load_data():
-    url = 'https://en.wikipedia.org/wiki/List_of_S%26P_500_companies'
-    html = pd.read_html(url, header=0)
-    df = html[0]
-    return df
+    # Import data from who_data.csv file and stores it as time_series
+    time_series = pd.read_csv('daily_covid_data.csv')
+    return time_series.dropna()
 
 
 df = load_data()
-sector = df.groupby('GICS Sector')
+country = df.groupby('Code')
+# Sidebar - Country selection
+sorted_country_unique = sorted(df['Entity'].unique())
+selected_country = st.sidebar.multiselect(
+    'Country', sorted_country_unique)
 
-# Sidebar - Sector selection
-sorted_sector_unique = sorted(df['GICS Sector'].unique())
-selected_sector = st.sidebar.multiselect(
-    'Sector', sorted_sector_unique, sorted_sector_unique)
 
 # Filtering data
-df_selected_sector = df[(df['GICS Sector'].isin(selected_sector))]
+df_selected_country = df[(df['Entity'].isin(selected_country))]
 
-st.header('Display Companies in Selected Sector')
-st.write('Data Dimension: ' + str(df_selected_sector.shape[0]) + ' rows and ' + str(
-    df_selected_sector.shape[1]) + ' columns.')
-st.dataframe(df_selected_sector)
-
-# Download S&P500 data
-# https://discuss.streamlit.io/t/how-to-download-file-in-streamlit/1806
+st.header('Display Selected Countries on 3/10/21')
+st.write('This will display the number of covid cases on 3/10/21')
+st.dataframe(df_selected_country.groupby('Entity').last())
 
 
 def filedownload(df):
     csv = df.to_csv(index=False)
     # strings <-> bytes conversions
     b64 = base64.b64encode(csv.encode()).decode()
-    href = f'<a href="data:file/csv;base64,{b64}" download="SP500.csv">Download CSV File</a>'
+    href = f'<a href="data:file/csv;base64,{b64}" download="Covid-19.csv">Download CSV File</a>'
     return href
 
 
-st.markdown(filedownload(df_selected_sector), unsafe_allow_html=True)
+st.markdown(filedownload(df_selected_country), unsafe_allow_html=True)
 
-# https://pypi.org/project/yfinance/
+data = df_selected_country
 
-data = yf.download(
-    tickers=list(df_selected_sector[:10].Symbol),
-    period="ytd",
-    interval="1d",
-    group_by='ticker',
-    auto_adjust=True,
-    prepost=True,
-    threads=True,
-    proxy=None
-)
-
-# Plot Closing Price of Query Symbol
+# Plot daily cases
 
 
-def price_plot(symbol):
-    df = pd.DataFrame(data[symbol].Close)
-    df['Date'] = df.index
-    plt.fill_between(df.Date, df.Close, color='skyblue', alpha=0.3)
-    plt.plot(df.Date, df.Close, color='skyblue', alpha=0.8)
+def daily_plot(code):
+    df = data[data['Code'] == code]
+    df.index = df['Day']
+    plt.plot(pd.to_datetime(df.Day), df['Daily new confirmed cases of COVID-19'],
+             color='skyblue', alpha=0.8)
     plt.xticks(rotation=90)
-    plt.title(symbol, fontweight='bold')
-    plt.xlabel('Date', fontweight='bold')
-    plt.ylabel('Closing Price', fontweight='bold')
+    plt.title(code)
+    plt.xlabel("Date")
+    plt.ylabel('Number of Daily Cases')
     return st.pyplot()
 
 
-num_company = st.sidebar.slider('Number of Companies', 1, 5)
-
 if st.button('Show Plots'):
-    st.header('Stock Closing Price')
-    for i in list(df_selected_sector.Symbol)[:num_company]:
-        price_plot(i)
+    st.header('Daily cases')
+    for i in list(df_selected_country.Code.unique()):
+        daily_plot(i)

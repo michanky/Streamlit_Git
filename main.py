46d04a07be0b7668b0b597d3901d9d8754b783b1
michanky <michaelngky@gmail.com>
2021-10-18 15:32:42 +0800

    Updated with WHO data instead

--- main.py
+++ main.py
@@ -16,24 +16,37 @@ st.sidebar.header('User Input Features')
 @st.cache
 def load_data():
     # Import data from who_data.csv file and stores it as time_series
-    time_series = pd.read_csv('daily_covid_data.csv')
+    # File is from World Health Organisation
+    time_series = pd.read_csv('WHO-COVID-19-global-data.csv')
+    time_series['Date_reported'] = pd.to_datetime(
+        time_series['Date_reported']).dt.date
     return time_series.dropna()
 
 
 df = load_data()
-country = df.groupby('Code')
+
+
 # Sidebar - Country selection
-sorted_country_unique = sorted(df['Entity'].unique())
+sorted_country_unique = sorted(df['Country'].unique())
 selected_country = st.sidebar.multiselect(
     'Country', sorted_country_unique)
 
+# Sidebar - Data selection
+min_date = df['Date_reported'].min()
+max_date = df['Date_reported'].max()
+
+a_date = st.sidebar.date_input("Pick a date", (min_date, max_date))
 
 # Filtering data
-df_selected_country = df[(df['Entity'].isin(selected_country))]
+date_mask = (df['Date_reported'] >= a_date[0]) & (
+    df['Date_reported'] <= a_date[1])
+df_selected_country = df[(df['Country'].isin(selected_country))]
+df_selected_country_and_date = df_selected_country[date_mask]
 
-st.header('Display Selected Countries on 3/10/21')
-st.write('This will display the number of covid cases on 3/10/21')
-st.dataframe(df_selected_country.groupby('Entity').last())
+st.header('Display Selected Countries')
+st.write('This will display the number of covid cases from ' +
+         str(a_date[0]) + ' to ' + str(a_date[1]))
+st.dataframe(df_selected_country_and_date)
 
 
 def filedownload(df):
@@ -44,20 +57,20 @@ def filedownload(df):
     return href
 
 
-st.markdown(filedownload(df_selected_country), unsafe_allow_html=True)
+st.markdown(filedownload(df_selected_country_and_date), unsafe_allow_html=True)
 
-data = df_selected_country
+data = df_selected_country_and_date
 
 # Plot daily cases
 
 
-def daily_plot(code):
-    df = data[data['Code'] == code]
-    df.index = df['Day']
-    plt.plot(pd.to_datetime(df.Day), df['Daily new confirmed cases of COVID-19'],
+def daily_plot(country):
+    df = data[data['Country'] == country]
+    df.index = df['Date_reported']
+    plt.plot(pd.to_datetime(df.Date_reported), df['New_cases'],
              color='skyblue', alpha=0.8)
     plt.xticks(rotation=90)
-    plt.title(code)
+    plt.title(country)
     plt.xlabel("Date")
     plt.ylabel('Number of Daily Cases')
     return st.pyplot()
@@ -65,5 +78,5 @@ def daily_plot(code):
 
 if st.button('Show Plots'):
     st.header('Daily cases')
-    for i in list(df_selected_country.Code.unique()):
+    for i in list(df_selected_country_and_date.Country.unique()):
         daily_plot(i)
